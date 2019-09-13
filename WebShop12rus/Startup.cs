01c8db8690c6b9f9b9eca2ec9169000c907b900d
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-09-13 09:43:42 +0300

    start

--- WebShop12rus/Startup.cs
+++ WebShop12rus/Startup.cs
@@ -5,16 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebShop12rus
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +33,29 @@ namespace WebShop12rus
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseMvcWithDefaultRoute();
+            // Конфигурация инфраструктуры MVC
+            app.UseMvc(routes =>
+            {
+                // Добавляем обработчик маршрута по умолчанию
+                routes.MapRoute(
+                     name: "default",
+                     template: "{controller=Home}/action=Index}/{id?}");
+                // Маршрут по умолчанию состоит из трех частей разделенных '/'
+                // Первой частью указывается имя контроллера,
+                // второй - имя действия (метода) в контроллере,
+                // третьей - опциональный параметр с именем 'id'
+                // Если часть не указана - используется значение по умолчанию:
+                // для контроллера имя 'Home'
+                // для действия - 'Index'
+            });
+
+            var helloMessage = Configuration["CustomHellowWorld"];
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(helloMessage);
             });
         }
     }
