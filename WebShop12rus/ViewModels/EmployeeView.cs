c7d9d318d3adad21d1041b943fd585fe66ec2cf7
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-09-13 12:18:20 +0300

    Created View: Index, Details Realized: GoBack from Details View

---
+++ WebShop12rus/ViewModels/EmployeeView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop12rus.ViewModels
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+    }
+}
